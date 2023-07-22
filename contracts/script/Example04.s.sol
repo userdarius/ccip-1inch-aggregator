3938f5470ea697096ed82073da054c69294c4b25
Darius Foodeei <darius.foodei@gmail.com>
2023-07-22 16:20:13 +0200

    scripts

---
+++ contracts/script/Example04.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "./Helper.sol";
+import {ProgrammableTokenTransfers} from "../src/ProgrammableTokenTransfers.sol";
+
+contract DeployProgrammableTokenTransfers is Script, Helper {
+    function run(SupportedNetworks network) external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        (address router,,,) = getConfigFromNetwork(network);
+
+        ProgrammableTokenTransfers programmableTokenTransfers = new ProgrammableTokenTransfers(
+                router
+            );
+
+        console.log(
+            "ProgrammableTokenTransfers contract deployed on ",
+            networks[network],
+            "with address: ",
+            address(programmableTokenTransfers)
+        );
+
+        vm.stopBroadcast();
+    }
+}
+
+contract SendTokensAndData is Script, Helper {
+    function run(address payable sender, SupportedNetworks destination, address receiver, address token, uint256 amount)
+        external
+    {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        (,,, uint64 destinationChainId) = getConfigFromNetwork(destination);
+
+        ProgrammableTokenTransfers(sender).initSwap(token, amount, destinationChainId, receiver);
+
+        console.log(
+            "You can now monitor the status of your Chainlink CCIP Message via https://ccip.chain.link using CCIP Message ID: "
+        );
+
+        vm.stopBroadcast();
+    }
+}
